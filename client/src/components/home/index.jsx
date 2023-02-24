c83eebfb1b542aebc49696d800361a46220f8558
Zhichao Han <zhichao@rightdevice.com>
2023-02-23 20:57:08 -0800

    Start putting in content

--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -4,6 +4,85 @@ import styles from './index.module.css';
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <div id="home" className="tatsu-SN6LuD0JDf tatsu-section  tatsu-bg-overlay   tatsu-clearfix" data-title="Home"  data-headerscheme="background--dark">
+       <div class='tatsu-section-pad clearfix' data-padding='{"d":"59px 0px 0px 0px","l":"61px 0px 0px 0px","t":"50px 50px 50px 50px","m":"100px 0px 100px 0px"}' data-padding-top='100px'>
+          <div className="tatsu-row-wrap  tatsu-wrap tatsu-row-has-one-half tatsu-row-has-two-cols tatsu-custom-gutter tatsu-reg-cols  tatsu-clearfix tatsu-5NGYDtkFgC" >
+             <div  className="tatsu-row HeroSectRow" style={{ marginLeft:"-0px", marginRight:"-0px" }}>
+                <div  className="tatsu-column  tatsu-bg-overlay tatsu-one-half tatsu-column-align-middle tatsu-column-image-none tatsu-column-effect-none  tatsu-V39RM5w-C"  data-parallax-speed="0" style={{ padding: "0 0px" }}>
+                   <div className="tatsu-column-inner " >
+                      <div className="tatsu-column-pad-wrap">
+                         <div className="tatsu-column-pad" >
+                            <div  className="tatsu-module tatsu-inline-text clearfix tatsu-nxSVzqt23   " >
+                               <div className="tatsu-inline-text-inner ">
+                                  <h1><span className="h1 ">#1 Lead Generation & Outreach Platform</span></h1>
+                               </div>
+                            </div>
+                            <div  className="tatsu-module tatsu-inline-text clearfix tatsu-OvcNgYbxL   " >
+                               <div className="tatsu-inline-text-inner ">
+                                  <p><span className="body ">Learn how we are revolutionizing the sales game by infusing traditional lead generation with powerful sales-AI to help reps unlock more opportunities, book more meetings, and crush their quota.</span></p>
+                               </div>
+                            </div>
+                            <div  className="tatsu-module tatsu-button-group align-left tatsu-SjhrPiYhPU  btnmainsec"  >
+                               <div  className="tatsu-module tatsu-normal-button tatsu-button-wrap   tatsu-cGqmz3dT2   btnmainsec">
+                                <a className="tatsu-shortcode mediumbtn tatsu-button left-icon    bg-animation-none  " href="signup/"  aria-label="Signup for free" data-gdpr-atts={{}} >Signup for free</a></div>
+                               <div id="gdpr-alt-lightbox-qC56hh8kiv" className=" white-popup mfp-hide" >
+                                  <div className="gdpr-alt-image">
+                                     <img style={{ opacity: "1", width: "100%" }} src="https://img.youtube.com/vi/7e90gBu4pas/maxresdefault.jpg"/>
+                                     <div className="gdpr-video-alternate-image-content" >Your consent is required to display this content from youtube - <a href="#gdpr-popup" className="mobx privacy-settings" data-type="HTML" >Privacy Settings</a> </div>
+                                  </div>
+                               </div>
+                               <div  className="tatsu-module tatsu-normal-button tatsu-button-wrap   tatsu-qC56hh8kiv   btnmainsec wthdemo">
+                                <a className="tatsu-shortcode mediumbtn tatsu-button left-icon rounded  mfp-iframe bg-animation-none be-gdpr-consent-required " href="https://www.youtube.com/watch?v=7e90gBu4pas"  aria-label="Watch demo" >
+                                <i className="tatsu-icon tatsu-icon-controller-play"></i>Watch demo</a>
+                              </div>
+                            </div>
+                            <ul  className="tatsu-module tatsu-list tatsu-6zVaGeCu4q   HeroSectionCheck tatsu-list-vertical-align-center tatsu-lists-icon" >
+                               <li  className="tatsu-list-content tatsu-blY2tTIkl   " >
+                                  <div className="tatsu-list-icon-wrap" ><i className="tatsu-icon tatsu-icon-check-square "></i></div>
+                                  <div className="tatsu-list-inner">
+                                     <p>30-day money-back guarantee</p>
+                                  </div>
+                               </li>
+                               <li  className="tatsu-list-content tatsu-KjiTfwAaN   " >
+                                  <div className="tatsu-list-icon-wrap" ><i className="tatsu-icon tatsu-icon-check-square "></i></div>
+                                  <div className="tatsu-list-inner">
+                                     <p>Monthly subscription, no commitment</p>
+                                  </div>
+                               </li>
+                            </ul>
+                         </div>
+                      </div>
+                      <div className="tatsu-column-bg-image-wrap">
+                         <div className="tatsu-column-bg-image" ></div>
+                      </div>
+                      <div className="tatsu-overlay tatsu-column-overlay tatsu-animate-none" ></div>
+                   </div>
+                </div>
+                <div  className="tatsu-column  tatsu-bg-overlay tatsu-one-half tatsu-column-image-none tatsu-column-effect-none imgtopsec tatsu-8oaWcx6yuD"  data-parallax-speed="0" style={{ padding: "0 0px" }}>
+                   <div className="tatsu-column-inner " >
+                      <div className="tatsu-column-pad-wrap">
+                         <div className="tatsu-column-pad" >
+                            <div  className="tatsu-single-image tatsu-module align-right tatsu-image-lazyload tatsu-image-overflow  tatsu-external-image tatsu-hV5cHO5mi  " >
+                               <div className="tatsu-single-image-inner " >
+                                  <div className="tatsu-single-image-padding-wrap" ></div>
+                                  <img className="tatsu-gradient-border" data-src = "https://spyrowptheme1.wpengine.com/saas-clickthrough-new/wp-content/uploads/sites/17/2020/12/Group-6945-1.png" alt =" "  src ="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="  />
+                               </div>
+                            </div>
+                         </div>
+                      </div>
+                      <div className="tatsu-column-bg-image-wrap">
+                         <div className="tatsu-column-bg-image" ></div>
+                      </div>
+                      <div className="tatsu-overlay tatsu-column-overlay tatsu-animate-none" ></div>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+       <div className="tatsu-section-background-wrap">
+          <div className="tatsu-section-background" ></div>
+       </div>
+       <div className="tatsu-overlay tatsu-section-overlay"></div>
+    </div>
   )
 }
