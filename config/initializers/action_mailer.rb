0a549ea7790138c1ec6b2c16e028bd762e2c9d11
Zhichao Han <zhichao@rightdevice.com>
2023-04-20 16:24:04 -0700

    Add sendgrid configs

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,11 +1,13 @@
 Rails.application.configure do
-  if ENV["SENDGRID_API_KEY"].present?
+  if ENV["SENDGRID_USERNAME"].present?
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
     config.action_mailer.smtp_settings = {
       address: "smtp.sendgrid.net",
       port: 587,
       authentication: :plain,
-      user_name: "apikey",
-      password: ENV["SENDGRID_API_KEY"],
+      user_name: ENV["SENDGRID_USERNAME"],
+      password: ENV["SENDGRID_PASSWORD"],
       domain: "salestiger.io",
       enable_starttls_auto: true
     }
