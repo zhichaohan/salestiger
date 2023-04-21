ba13d7f6ff78ee3a32008f442fadabd2898b9553
Zhichao Han <zhichao@rightdevice.com>
2023-04-21 13:10:54 -0700

    Fix sync_last_sent_email

--- app/models/account_lead.rb
+++ app/models/account_lead.rb
@@ -7,6 +7,6 @@ class AccountLead < ApplicationRecord
   def sync_last_sent_email!
     account_team_members = self.account.team_members
 
-    self.update!(last_sent_email: self.lead.emails.where(team_member: account_team_members).newest_first.first)
+    self.update!(last_sent_email: self.lead.emails.sent.where(team_member: account_team_members).newest_first.first)
   end
 end
