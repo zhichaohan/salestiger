7dc4ec2f469c9f5c07c4656b96970e58c65e0c72
Zhichao Han <zhichao@rightdevice.com>
2023-05-16 11:54:01 -0700

    Add log

--- app/workers/team_members/gmail_subscribe_worker.rb
+++ app/workers/team_members/gmail_subscribe_worker.rb
@@ -5,6 +5,7 @@ class TeamMembers::GmailSubscribeWorker
 
   def perform
     TeamMember.email_activated.each do |team_member|
+      SlackService.notify_product_notifications("Set up for team member #{team_member.gmail}")
       team_member.setup_gmail_watcher!
     end
   end
