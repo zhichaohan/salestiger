eefaad632cdb9cfb63ee79b7bb928aa8bf317912
Zhichao Han <zhichao@rightdevice.com>
2023-05-16 09:52:54 -0700

    Fix testing code

--- app/workers/emails/send_worker.rb
+++ app/workers/emails/send_worker.rb
@@ -12,13 +12,13 @@ class Emails::SendWorker
     email = Email.find_by(id: id)
     return unless email.present?
 
-    # if ENV['RAILS_ENV'] != 'production'
-    #   email.update!(
-    #     status: 'sent',
-    #     sent_at: DateTime.now
-    #   )
-    #   email.update_account_lead_status!
-    # end
+    if ENV['RAILS_ENV'] != 'production'
+      email.update!(
+        status: 'sent',
+        sent_at: DateTime.now
+      )
+      email.update_account_lead_status!
+    end
 
     return unless email.team_member.auth_token.present?
 
