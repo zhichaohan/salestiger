7951ba8b88a75b74e73abd14e2a5eb641d476d52
Zhichao Han <zhichao@rightdevice.com>
2023-05-23 10:54:44 -0700

    Update slack message

--- app/models/lead_linkedin_sequence.rb
+++ app/models/lead_linkedin_sequence.rb
@@ -4,6 +4,6 @@ class LeadLinkedinSequence < ApplicationRecord
   belongs_to :team_member
 
   def notify!
-    SlackService.notify_linkedin_bot("Lead ID: #{lead.id} has been added to sequence: #{linkedin_sequence.id}")
+    SlackService.notify_linkedin_bot("Lead ID: #{lead.id} has been added to sequence: #{linkedin_sequence.id}\nTeam Member: #{team_member.name}\nLead Linkedin URL: #{lead.linkedin_url}")
   end
 end
